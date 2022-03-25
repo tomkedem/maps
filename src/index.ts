40114c9ea1b1bf579cf7d43cee7a826450861f99
Tom Kedem <tompha1@hotmail.com>
2022-03-25 16:18:40 +0300

    starting map

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hi Tomer')
+// import { User } from './User'
+// import { Company } from './Company'
+
+// const user = new User()
+
+// console.log(user)
+
+
+// const company = new Company()
+
+// console.log(company)
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 3,
+  center:{
+    lat:0,
+    lng:0
+  }
+})
+
